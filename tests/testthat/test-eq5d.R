fe7d9e1862c1811e32664e9e5a864c9c2ff9fde5
Fraser Morton <fraser.morton@glasgow.ac.uk>
2019-12-12 12:17:11 +0000

    Unit tests for custom column names.

--- tests/testthat/test-eq5d.R
+++ tests/testthat/test-eq5d.R
@@ -12,6 +12,7 @@ test_that("eq5d throws error for incorrect parameters", {
 
 test.df <- data.frame(MO=c(1,2,3,4,5),SC=c(1,5,4,3,2),UA=c(1,5,2,3,1),PD=c(1,3,4,3,4),AD=c(1,2,1,2,1))
 test.df2 <- data.frame(state=c(11111,25532,34241,43332,52141))
+test.df3 <- data.frame(Mobility=c(1,2,3,4,5),Care=c(1,5,4,3,2),Activities=c(1,5,2,3,1),Pain=c(1,3,4,3,4),Anxiety=c(1,2,1,2,1))
 
 test_that("Wrapper function gives correct answer", {
   expect_equal(eq5d(c(MO=1,SC=2,UA=3,PD=2,AD=1), type="TTO", version="3L", country="UK"), 0.329)
@@ -20,8 +21,9 @@ test_that("Wrapper function gives correct answer", {
   expect_equal(eq5d(c(MO=1,SC=2,UA=3,PD=4,AD=5), country="Indonesia", version="5L", type="VT"), 0.240)
   expect_equal(eq5d(test.df, country="Canada", version="5L", type="VT"), c(0.949,0.362,0.39,0.524,0.431))
   expect_equal(eq5d(test.df, country="England", version="5L", type="VT"), c(1.000,0.393,0.434,0.488,0.400))
-  expect_equal(eq5d(test.df2, country="Canada", version="5L", type="VT"), c(0.949,0.362,0.39,0.524,0.431))
-  expect_equal(eq5d(test.df2, country="England", version="5L", type="VT"), c(1.000,0.393,0.434,0.488,0.400))
+  expect_equal(eq5d(test.df3, country="England", version="5L", type="VT", dimensions=c("Mobility", "Care", "Activities", "Pain", "Anxiety")), c(1.000,0.393,0.434,0.488,0.400))
+  expect_equal(eq5d(test.df2, country="Canada", version="5L", type="VT", five.digit="state"), c(0.949,0.362,0.39,0.524,0.431))
+  expect_equal(eq5d(test.df2, country="England", version="5L", type="VT", five.digit="state"), c(1.000,0.393,0.434,0.488,0.400))
   expect_equal(eq5d(c(MO=1,SC=2,UA=3,PD=2,AD=1), type="CW", version="5L", country="Denmark"), 0.736)
   expect_equal(eq5d(12345, country="Indonesia", version="5L", type="VT"), 0.240)
 })
