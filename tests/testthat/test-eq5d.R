db0467717f147bf78a568c0b35964bba814f9d8e
Fraser Morton <fraser.morton@glasgow.ac.uk>
2019-12-14 23:02:39 +0000

    Added additional test for eq5d function dimensions parameter.

--- tests/testthat/test-eq5d.R
+++ tests/testthat/test-eq5d.R
@@ -7,7 +7,6 @@ test_that("eq5d throws error for incorrect parameters", {
   expect_error(eq5d("12345", version="5L", type="VT", country="England"))
   expect_error(eq5d(10000, version="5L", type="VT", country="England"))
   expect_error(eq5d(99999, version="5L", type="VT", country="England"))
-  
 })
 
 test.df <- data.frame(MO=c(1,2,3,4,5),SC=c(1,5,4,3,2),UA=c(1,5,2,3,1),PD=c(1,3,4,3,4),AD=c(1,2,1,2,1))
@@ -26,6 +25,7 @@ test_that("Wrapper function gives correct answer", {
   expect_equal(eq5d(test.df2, country="England", version="5L", type="VT", five.digit="state"), c(1.000,0.393,0.434,0.488,0.400))
   expect_equal(eq5d(c(MO=1,SC=2,UA=3,PD=2,AD=1), type="CW", version="5L", country="Denmark"), 0.736)
   expect_equal(eq5d(12345, country="Indonesia", version="5L", type="VT"), 0.240)
+  expect_error(eq5d(test.df, version="3L", type="TTO", country="USA", dimensions=c("Mob", "Self", "Active", "Pain", "Anx")))
 })
 
 test_that("when ignore.incomplete flag is TRUE the correct answer is returned", {
@@ -41,6 +41,5 @@ test_that("when ignore.incomplete flag is FALSE the correct answer is returned",
   expect_error(eq5d("12321", type="TTO", version="3L", country="UK", ignore.incomplete=FALSE))
   expect_error(eq5d(1232, type="TTO", version="3L", country="UK", ignore.incomplete=FALSE))
   expect_error(eq5d(NA_integer_, type="TTO", version="3L", country="UK", ignore.incomplete=FALSE))
-  
 })
 
