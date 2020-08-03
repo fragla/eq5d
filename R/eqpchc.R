9c9e6041827600bbdf5c873f3fba6ec6ec398516
Fraser Morton <fraser.morton@glasgow.ac.uk>
2020-08-03 09:28:47 +0100

    Added pchc function, docs and tests.

---
+++ R/eqpchc.R
@@ -0,0 +1,149 @@
+#' Calculate the Paretian Classification of Health Change
+#' 
+#' Calculate the Paretian Classification of Health Change (PCHC) for two EQ-5D datasets.
+#' 
+#' @param pre data.frame, numeric or character. For data.frame default column 
+#' names should be MO, SC, UA, PD and AD representing Mobility, Self-care, 
+#' Usual activities, Pain/discomfort and Anxiety/depression. Vector using five 
+#' digit format can also be used.
+#' @param post data.frame, numeric or character. For data.frame default column 
+#' names should be MO, SC, UA, PD and AD representing Mobility, Self-care, 
+#' Usual activities, Pain/discomfort and Anxiety/depression. Vector using five 
+#' digit format can also be used.
+#' @param version string of value "3L" or "5L" to indicate instrument version.
+#' @param no.problems boolean. Summarise 11111 "No change" subjects in a "No problems" 
+#' group.
+#' @param totals boolean. Include a summary total.
+#' @param by.dimension boolean. Summarise results by each EQ-5D dimension rather 
+#' than by the whole dataset.
+#' @param ignore.invalid boolean whether to ignore invalid scores. TRUE returns NA, FALSE 
+#' throws an error.
+#' @param dimensions character vector, specifying "dimension" column names. Defaults 
+#' are "MO", "SC", "UA", "PD" and "AD".
+#' @return a data.frame or list of data.frames of changes according to PCHC.
+#' contain dimensions names and rows the EQ-5D score.
+#' @export
+pchc <- function(pre, post, version="5L", no.problems=TRUE, totals=TRUE, by.dimension=FALSE, ignore.invalid=TRUE, dimensions=.getDimensionNames()) {
+  
+  if(is.character(pre) || is.numeric(pre)) {
+    pre <- splitHealthStates(pre, version=version, ignore.invalid=ignore.invalid)
+  }
+
+  if(is.character(post) || is.numeric(post)) {
+    post <- splitHealthStates(post, version=version, ignore.invalid=ignore.invalid)
+  }
+
+  if(all(dimensions %in% names(pre)) && all(dimensions %in% names(post))) {
+    pre <- pre[,dimensions]
+    colnames(pre) <- .getDimensionNames()
+    post <- post[,dimensions]
+    colnames(post) <- .getDimensionNames()
+  } else {
+    stop("Unable to identify EQ-5D dimensions in data.frames.")
+  }
+  
+  pre.idx <- which(apply(pre, 1, function(x) { any(!x%in% 1:sub("L", "", version))}))
+  post.idx <- which(apply(post, 1, function(x) { any(!x%in% 1:sub("L", "", version))}))
+  
+  if(length(pre.idx)>0 || length(post.idx)>0) {
+    if(ignore.invalid) {
+      pre <- pre[-c(pre.idx,post.idx),]
+      post <- post[-c(pre.idx,post.idx),]
+    } else {
+      stop("Missing/non-numeric dimension found.")
+    }
+  }
+  # if(any(!unlist(pre) %in% 1:sub("L", "", version))) {
+  #   if(ignore.invalid) {
+  #     pre <- apply(post, 2, function(x) ifelse(x %in% 1:sub("L", "", version), NA, x))
+  #   } else {
+  #     stop("Missing/non-numeric dimension found in pre.")
+  #   }
+  # }
+  # 
+  # if(any(!unlist(post) %in% 1:sub("L", "", version))) {
+  #   if(ignore.invalid) {
+  #     post <- apply(post, 2, function(x) ifelse(x %in% 1:sub("L", "", version), NA, x))
+  #   } else {
+  #     stop("Missing/non-numeric dimension found in post.")
+  #   }
+  # }
+  
+  ##check lengths
+  
+  if(!by.dimension) {
+    res <- .pchc(pre, post, no.problems, totals)
+  } else {
+    res <- lapply(.getDimensionNames(), function(x) {
+      .pchc(pre[,x, drop=FALSE], post[,x, drop=FALSE], no.problems, totals)
+    })
+    names(res) <- .getDimensionNames()
+  }
+  return(res)
+}
+
+.pchc <- function(pre, post, no.problems, totals) {
+  res <- sapply(1:nrow(pre), function(x) {
+    .classif(pre[x,], post[x,], no.problems=no.problems)
+  })
+  
+  if(no.problems) {
+    .total.no.problems(res, totals)
+  } else {
+    .total(res, totals)
+  }
+}
+
+.total.no.problems <- function(res, totals=TRUE) {
+  res <- table(res)
+  res.df <- data.frame(row.names=c("No change", "Improve", "Worsen", "Mixed change", 
+                                "Total with problems", "No problems"),
+                    Number=rep(0,6), Percent=rep(0,6))
+  idx <- match(names(res), rownames(res.df))
+  res.df$Number[idx] <- res
+  
+  if(totals) {
+    problem.idx <- match(c("No change", "Improve", "Worsen", "Mixed change"), rownames(res.df))
+    totals.idx <- match(c("Total with problems", "No problems"), rownames(res.df))
+    
+    res.df["Total with problems","Number"] <- sum(res.df[problem.idx,"Number"])
+    res.df[problem.idx,"Percent"] <- round(prop.table(res.df[problem.idx,"Number"])*100,1)
+    res.df[totals.idx,"Percent"] <- round(prop.table(res.df[totals.idx,"Number"])*100,1)
+  } else {
+    res.df$Percent <- round(prop.table(res.df$Number)*100,1)
+  }
+  return(res.df)
+}
+
+.total <- function(res, totals=TRUE){
+  res <- table(res)
+  res.df <- data.frame(row.names=c("No change", "Improve", "Worsen", "Mixed change"), 
+                       Number=rep(0,4))
+  idx <- match(names(res), rownames(res.df))
+  res.df$Number[idx] <- res
+  res.df$Percent <- prop.table(res.df$Number)
+  if(totals) {
+    res.df["Total",] <- apply(res.df, 2, sum)
+  }
+  res.df$Percent <- round(res.df$Percent*100,1)
+  return(res.df)
+}
+
+.classif <- function(pre, post, no.problems=TRUE) {
+  improve <- any(pre > post)
+  worsen <- any(pre < post)
+
+  if(improve && worsen) {
+    return("Mixed change")
+  } else if(!improve && !worsen) {
+    if(all(pre==1) && no.problems) {
+      return("No problems")
+    } else {
+      return("No change")
+    }
+  } else if(improve) {
+    return("Improve")
+  } else if(worsen) {
+    return("Worsen")
+  }
+}
---
+++ tests/testthat/test-eqpchc.R
@@ -0,0 +1,41 @@
+context("EQ-5D PCHC")
+
+pre <- read.csv("../testdata/pre.csv")$x
+post <- read.csv("../testdata/post.csv")$x
+
+res1 <- read.csv("../testdata/pchc_noprob_true_totals_true.csv", row.names=1, colClasses=c("character",rep("numeric",2)))
+res2 <- read.csv("../testdata/pchc_noprob_true_totals_false.csv", row.names=1, colClasses=c("character",rep("numeric",2)))
+res3 <- read.csv("../testdata/pchc_noprob_false_totals_false.csv", row.names=1, colClasses=c("character",rep("numeric",2)))
+res4 <- read.csv("../testdata/pchc_noprob_false_totals_true.csv", row.names=1, colClasses=c("character",rep("numeric",2)))
+
+res1.mo <- read.csv("../testdata/pchc_noprob_true_totals_true_dim_mo.csv", row.names=1, colClasses=c("character",rep("numeric",2)))
+res2.mo <- read.csv("../testdata/pchc_noprob_true_totals_false_dim_mo.csv", row.names=1, colClasses=c("character",rep("numeric",2)))
+res3.mo <- read.csv("../testdata/pchc_noprob_false_totals_false_dim_mo.csv", row.names=1, colClasses=c("character",rep("numeric",2)))
+res4.mo <- read.csv("../testdata/pchc_noprob_false_totals_true_dim_mo.csv", row.names=1, colClasses=c("character",rep("numeric",2)))
+
+test_that("eqpchc five digit gives correct answer", {
+  expect_equal(pchc(pre, post, version="3L", no.problems=TRUE, totals=TRUE), res1)
+  expect_equal(pchc(pre, post, version="3L", no.problems=TRUE, totals=FALSE), res2)
+  expect_equal(pchc(pre, post, version="3L", no.problems=FALSE, totals=FALSE), res3)
+  expect_equal(pchc(pre, post, version="3L", no.problems=FALSE, totals=TRUE), res4)
+  expect_equal(pchc(pre, post, version="3L", no.problems=TRUE, totals=TRUE, by.dimension=TRUE)$MO, res1.mo)
+  expect_equal(pchc(pre, post, version="3L", no.problems=TRUE, totals=FALSE, by.dimension=TRUE)$MO, res2.mo)
+  expect_equal(pchc(pre, post, version="3L", no.problems=FALSE, totals=FALSE, by.dimension=TRUE)$MO, res3.mo)
+  expect_equal(pchc(pre, post, version="3L", no.problems=FALSE, totals=TRUE, by.dimension=TRUE)$MO, res4.mo)
+})
+
+pre.df <- read.csv("../testdata/pre_df.csv")
+post.df <- read.csv("../testdata/post_df.csv")
+
+test_that("eqpchc data.frame gives correct answer", {
+  expect_equal(pchc(pre.df, post.df, version="3L", no.problems=TRUE, totals=TRUE), res1)
+  expect_equal(pchc(pre.df, post.df, version="3L", no.problems=TRUE, totals=FALSE), res2)
+  expect_equal(pchc(pre.df, post.df, version="3L", no.problems=FALSE, totals=FALSE), res3)
+  expect_equal(pchc(pre.df, post.df, version="3L", no.problems=FALSE, totals=TRUE), res4)
+})
+
+test_that("eqpchc data.frame throws error", {
+  expect_error(pchc(pre.df, post.df, version="3L", dimensions=c("M0","SC","UA","PD","AD")))
+  expect_error(pchc(pre.df, post.df, version="3L", ignore.invalid=FALSE))
+})
+
