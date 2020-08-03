494c3a9f23294768893506e0d7b1a0e996a64498
Fraser Morton <fraser.morton@glasgow.ac.uk>
2020-08-03 09:28:01 +0100

    Added eq5dcf cumulative freq function, docs and tests.

---
+++ R/eq5dcf.R
@@ -0,0 +1,58 @@
+#' Calculate the cumulative frequency profile of an EQ-5D dataset
+#' 
+#' Calculate the frequency, percentage, cumulative frequency and cumulative 
+#' percentage for each profile in an EQ-5D dataset.
+#' 
+#' @param data data.frame with names MO, SC, UA, PD and AD representing
+#'   Mobility, Self-care, Usual activities, Pain/discomfort and Anxiety/depression.
+#' @param version string of value "3L" or "5L" to indicate instrument version.
+#' @param ignore.invalid whether to ignore invalid scores. TRUE returns NA, FALSE throws an 
+#' error.
+#' @param ... character vector, specifying "dimensions" column names. Defaults 
+#' are "MO", "SC", "UA", "PD" and "AD".
+#' @return a data.frame or list of data.frames of counts/percentages. Columns 
+#' contain dimensions names and rows the EQ-5D score.
+#' @export
+eq5dcf <- function(data, version, ignore.invalid, ...) {
+  UseMethod("eq5dcf", data)
+}
+
+#' @export
+eq5dcf.data.frame <- function(data, version, ignore.invalid=TRUE, ...) {
+  args <- list(...)
+  
+  dimensions <- .getDimensionNames()
+  if(!is.null(args$dimensions)) {dimensions <- args$dimensions}
+  
+  states <- getHealthStatesFromDimensions(data, version, ignore.invalid, dimensions)
+  eq5dcf.default(states, version, ignore.invalid, ...)
+}
+
+#' @export
+eq5dcf.matrix <- function(data, version, ignore.invalid=TRUE, ...) {
+  data <- as.data.frame(data)
+  eq5dcf.data.frame(data, version, ignore.invalid=TRUE, ...)
+}
+
+#' @export
+eq5dcf.default <- function(data, version, ignore.invalid=TRUE, ...) {
+  invalid.idx <- which(!data %in% getHealthStates(version))
+  
+  if(length(invalid.idx) > 0) {
+    if(ignore.invalid) {
+      data[invalid.idx] <- NA
+    } else {
+      stop("Invalid dimension state(s) found.")
+    }
+  } 
+
+  #State, Frequency, Percentage, Cumulative frequency, Cumulative percentage
+  frequencies <- sort(table(data), decreasing=TRUE)
+  percentage <- round(prop.table(as.numeric(frequencies))*100,1)
+  cum.freq <- cumsum(as.numeric(frequencies))
+  cum.perc <- round(cum.freq/sum(frequencies)*100,1)
+  
+  return(data.frame(State=names(frequencies), Frequency=as.numeric(frequencies),
+                    Percentage=percentage, CumulativeFreq=cum.freq, CumulativePerc=cum.perc))
+}
+
---
+++ tests/testthat/test-eq5dcf.R
@@ -0,0 +1,32 @@
+context("EQ-5D Cumulative Frequency")
+
+dat <- read.csv("../testdata/pre.csv")$x
+res <- read.csv("../testdata/eq5dcf.csv", colClasses=c("character",rep("numeric",4)))
+
+test_that("eq5dcf five digit gives correct answer", {
+  expect_equal(eq5dcf(dat, version="3L", ignore.invalid=TRUE), res)
+})
+
+test_that("eq5dcf five digit throws error", {
+  expect_error(eq5dcf(dat, version="3L", ignore.invalid=FALSE))
+  
+  #expect_error(eq5dds(test1.df, version="3L", by="Gender"))
+})
+
+dat.df <- read.csv("../testdata/pre_df.csv")
+dat.df.nam <- data.frame(Mob=dat.df$MO, SelCa=dat.df$SC, UsAct=dat.df$UA,PDis=dat.df$PD, AnxDep=dat.df$AD)
+
+test_that("eq5dcf data.frame gives correct answer", {
+  expect_equal(eq5dcf(dat.df, version="3L", ignore.invalid=TRUE), res)
+  expect_equal(eq5dcf(dat.df.nam, version="3L", ignore.invalid=TRUE, dimensions=c("Mob","SelCa","UsAct","PDis","AnxDep")), res)
+})
+
+test_that("eq5dcf data.frame throws error", {
+  expect_error(eq5dcf(dat.df, version="3L", ignore.invalid=FALSE))
+  expect_error(eq5dcf(dat, version="3L", ignore.invalid=FALSE, dimensions=c("MO", "SC", "UA", "Pa", "AD")))
+})
+
+dat.mat <- as.matrix(dat.df)
+test_that("eq5dcf data.frame gives correct answer", {
+  expect_equal(eq5dcf(dat.mat, version="3L", ignore.invalid=TRUE), res)
+})
