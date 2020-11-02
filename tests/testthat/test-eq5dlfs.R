abe6c9605f47ee94b84a452b74e58aefffc3ea82
Fraser Morton <fraser.morton@glasgow.ac.uk>
2020-11-02 14:23:16 +0000

    Additional tests added for lfs and lss functions.

--- tests/testthat/test-eq5dlfs.R
+++ tests/testthat/test-eq5dlfs.R
@@ -9,16 +9,33 @@ test_that("single lfs gives correct answer", {
 test_that("five digit lfs gives correct answer", {
   expect_equal(lfs(12321, version="3L"), "221")
   expect_equal(lfs(55555, version="5L"), "00005")
+  expect_equal(lfs(55575, version="5L", ignore.invalid=TRUE), NA)
   expect_equal(lfs(c(11111,12345, 55555), version="5L"), c("50000","11111","00005"))
+  expect_equal(lfs(as.character(c(11111,12345, 55555)), version="5L"), c("50000","11111","00005"))
 })
 
 test.df <- data.frame(MO=c(1,2,3,4,5),SC=c(1,5,4,3,2),UA=c(1,5,2,3,1),PD=c(1,3,4,3,4),AD=c(1,2,1,2,1))
+test.df.2 <- test.df
+colnames(test.df.2) <- c("M", "S", "U", "P", "A")
 
 test_that("data.frame lfs gives correct answer", {
   expect_equal(lfs(test.df, version="5L"), c("50000","02102","11120","01310","21011"))
+  expect_equal(lfs(test.df.2, version="5L", dimensions=c("M","S","U","P","A")), c("50000","02102","11120","01310","21011"))
+})
+
+test_that("matrix lfs gives correct answer", {
+  expect_equal(lfs(as.matrix(test.df), version="5L"), c("50000","02102","11120","01310","21011"))
+})
+
+test_that("five digit in data.fram lss gives correct answer", {
+  expect_equal(lfs(data.frame(five.digit=c(11111,12345, 55555)), version="5L", five.digit="five.digit"), c("50000","11111","00005"))
 })
 
 test_that("lfs throws error for incorrect parameters", {
   expect_error(lfs(12345, version="3L", ignore.invalid=FALSE))
   expect_error(lfs(c(MO=1,SC=7,UA=3,PD=2,AD=1), version="3L", ignore.invalid=FALSE))
+  expect_error(lfs(data.frame(five.digit=c(11111,12345, 55555)), version="5L"))
+  expect_error(lfs(data.frame(five.digit=c(11111,12345, 55555)), version="7L"))
+  expect_error(lss(data.frame(five.digit=c(11111,12345, 55555)), version="5L", dimensions=c("M","S","U","P","A")))
+  
 })
--- tests/testthat/test-eq5dlss.R
+++ tests/testthat/test-eq5dlss.R
@@ -9,16 +9,32 @@ test_that("single lss gives correct answer", {
 test_that("five digit lss gives correct answer", {
   expect_equal(lss(12321, version="3L"), 9)
   expect_equal(lss(55555, version="5L"), 25)
+  expect_equal(lfs(55575, version="5L", ignore.invalid=TRUE), NA)
   expect_equal(lss(c(11111,12345, 55555), version="5L"), c(5,15,25))
+  expect_equal(lss(as.character(c(11111,12345, 55555)), version="5L"), c(5,15,25))
 })
 
 test.df <- data.frame(MO=c(1,2,3,4,5),SC=c(1,5,4,3,2),UA=c(1,5,2,3,1),PD=c(1,3,4,3,4),AD=c(1,2,1,2,1))
+test.df.2 <- test.df
+colnames(test.df.2) <- c("M", "S", "U", "P", "A")
 
 test_that("data.frame lss gives correct answer", {
   expect_equal(lss(test.df, version="5L"), c(5,17,14,15,13))
+  expect_equal(lss(test.df.2, version="5L", dimensions=c("M","S","U","P","A")), c(5,17,14,15,13))
+})
+
+test_that("matrix lss gives correct answer", {
+  expect_equal(lss(as.matrix(test.df), version="5L"), c(5,17,14,15,13))
+})
+
+test_that("five digit in data.fram lss gives correct answer", {
+  expect_equal(lss(data.frame(five.digit=c(11111,12345, 55555)), version="5L", five.digit="five.digit"), c(5,15,25))
 })
 
 test_that("lss throws error for incorrect parameters", {
   expect_error(lss(12345, version="3L", ignore.invalid=FALSE))
   expect_error(lss(c(MO=1,SC=7,UA=3,PD=2,AD=1), version="3L", ignore.invalid=FALSE))
+  expect_error(lss(data.frame(five.digit=c(11111,12345, 55555)), version="5L"))
+  expect_error(lss(data.frame(five.digit=c(11111,12345, 55555)), version="7L"))
+  expect_error(lss(data.frame(five.digit=c(11111,12345, 55555)), version="5L", dimensions=c("M","S","U","P","A")))
 })
