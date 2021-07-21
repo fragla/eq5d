447bfdb8e1d7cc7c4c867578681afea4dfcc4d9a
Fraser Morton <fraser.morton@glasgow.ac.uk>
2021-07-21 13:07:50 +0100

    Reverse crosswalk function, data (England, Germany, Netherlands, USA), tests and docs added.

---
+++ R/eq5drcw.R
@@ -0,0 +1,29 @@
+#' Calculate EQ-5D-3L reverse crosswalk index scores
+#' 
+#' Calculate indices for EQ-5D-3L indices by mapping them onto EQ-5D-5L value sets. 
+#' Available value sets can be viewed using the function \code{valuesets}.
+#' 
+#' @param scores numeric with names MO, SC, UA, PD and AD representing
+#'   Mobility, Self-care, Usual activities, Pain/discomfort and Anxiety/depression.
+#' @param country value set country.
+#' @return calculated utility index score. 
+#' @examples
+#' eq5drcw(scores=c(MO=1,SC=2,UA=3,PD=2,AD=1), country="Netherlands")
+#' eq5drcw(scores=c(MO=3,SC=3,UA=3,PD=3,AD=3), country="Germany")
+#' 
+#' @export
+eq5drcw <- function(scores, country="UK") {  
+  if(!all(.getDimensionNames() %in% names(scores))) {
+    stop("Unable to identify EQ-5D dimensions in scores.")
+  }
+  
+  if(!all(scores %in% 1:3))
+    stop("Scores must be coded as 1, 2, or 3 for EQ-5D-3L Reverse Crosswalk.")
+  
+  survey <- get("RCW")
+  
+  if(is.null(country) || !country %in% colnames(survey))
+    stop(paste("For EQ-5D-3L reverse crosswalk value sets country must be one of:", paste(colnames(survey), collapse=", ")))
+  
+  return(survey[paste(scores, collapse=""), country])
+}
---
+++ tests/testthat/test-eq5drcw.R
@@ -0,0 +1,36 @@
+context("EQ-5D-3L Reverse Crosswalk")
+
+test_that("EQ-5D-5L England Crosswalk gives correct answer", {
+  expect_equal(eq5drcw(c(MO=1,SC=1,UA=1,PD=1,AD=1), "England"), 0.965)
+  expect_equal(eq5drcw(c(MO=2,SC=2,UA=2,PD=2,AD=2), "England"), 0.540)
+  expect_equal(eq5drcw(c(MO=3,SC=2,UA=1,PD=2,AD=3), "England"), 0.289)
+  expect_equal(eq5drcw(c(MO=3,SC=3,UA=3,PD=3,AD=3), "England"), -0.172)
+})
+
+test_that("EQ-5D-5L Germany Crosswalk gives correct answer", {
+  expect_equal(eq5drcw(c(MO=1,SC=1,UA=1,PD=1,AD=1), "Germany"), 0.977)
+  expect_equal(eq5drcw(c(MO=2,SC=2,UA=2,PD=2,AD=2), "Germany"), 0.620)
+  expect_equal(eq5drcw(c(MO=3,SC=2,UA=1,PD=2,AD=3), "Germany"), 0.332)
+  expect_equal(eq5drcw(c(MO=3,SC=3,UA=3,PD=3,AD=3), "Germany"), -0.329)
+})
+
+test_that("EQ-5D-5L Netherlands Crosswalk gives correct answer", {
+  expect_equal(eq5drcw(c(MO=1,SC=1,UA=1,PD=1,AD=1), "Netherlands"), 0.947)
+  expect_equal(eq5drcw(c(MO=2,SC=2,UA=2,PD=2,AD=2), "Netherlands"), 0.486)
+  expect_equal(eq5drcw(c(MO=3,SC=2,UA=1,PD=2,AD=3), "Netherlands"), 0.215)
+  expect_equal(eq5drcw(c(MO=3,SC=3,UA=3,PD=3,AD=3), "Netherlands"), -0.312)
+})
+
+test_that("EQ-5D-5L USA Crosswalk gives correct answer", {
+  expect_equal(eq5drcw(c(MO=1,SC=1,UA=1,PD=1,AD=1), "USA"), 0.962)
+  expect_equal(eq5drcw(c(MO=2,SC=2,UA=2,PD=2,AD=2), "USA"), 0.417)
+  expect_equal(eq5drcw(c(MO=3,SC=2,UA=1,PD=2,AD=3), "USA"), 0.176)
+  expect_equal(eq5drcw(c(MO=3,SC=3,UA=3,PD=3,AD=3), "USA"), -0.422)
+})
+
+context("EQ-5D-3L Reverse Crosswalk Incorrect params")
+
+test_that("EQ-5D-3L crosswalk throws error for incorrect parameters", {
+  expect_error(eq5drcw(c(MD=5,SC=5,UA=5,PD=5,AD=5), "Germany"))
+  expect_error(eq5drcw(c(MO=1,SC=1,UA=1,PD=1,AD=1), "Swaziland"))
+})
