369b679c353de2d24f52eb2e8c6d060030d74a7c
Fraser Morton <fraser.morton@glasgow.ac.uk>
2023-01-12 09:54:54 +0000

    Additional HPG and PS tests added.

--- tests/testthat/test-eqhpg.R
+++ tests/testthat/test-eqhpg.R
@@ -3,6 +3,10 @@ context("EQ-5D HPG")
 pre <- read.csv("../testdata/pre.csv")$x
 post <- read.csv("../testdata/post.csv")$x
 
+pre.df <- read.csv("../testdata/pre_df.csv")
+post.df <- read.csv("../testdata/post_df.csv")
+
+
 res <- read.csv("../testdata/hpg_3l_uk_tto_no_problems_false.csv")
 
 test_that("eqhpg five digit gives correct answer", {
@@ -10,9 +14,11 @@ test_that("eqhpg five digit gives correct answer", {
 })
 
 test_that("eqhpg data.frame gives correct answer", {
-  expect_equal(hpg(getDimensionsFromHealthStates(pre), getDimensionsFromHealthStates(post), country = "UK", version="3L", type="TTO", no.problems = F), res)
+  expect_equal(hpg(pre.df, post.df, country = "UK", version="3L", type="TTO", no.problems = F), res)
 })
 
 test_that("eqhpg data.frame throws error", {
-  expect_error(hpg(pre, post, country = "UK", version="3L", type="TTO", ignore.invalid = FALSE))
+  expect_error(hpg(pre.df, post, country = "UK", version="3L", type="TTO", ignore.invalid = FALSE))
+  expect_error(hpg(pre.df, post.df, country = "UK", version="3L", type="TTO", dimensions=c("M0","SC","UA","PD","AD")))
+  expect_error(hpg(pre.df[-1,], post.df, country = "UK", version="3L", type="TTO", ignore.invalid=TRUE))
 })
--- tests/testthat/test-eqps.R
+++ tests/testthat/test-eqps.R
@@ -1,9 +1,9 @@
-context("EQ-5D PCHC")
+context("EQ-5D Probability of Superiority")
 
 pre <- read.csv("../testdata/pre.csv")$x
 post <- read.csv("../testdata/post.csv")$x
 
-test_that("eqpchc five digit gives correct answer", {
+test_that("eqps five digit gives correct answer", {
   expect_equal(ps(pre, post, version="3L", ignore.invalid=TRUE)$MO, 0.59)
   expect_equal(ps(pre, post, version="3L", ignore.invalid=TRUE)$SC, 0.62)
   expect_equal(ps(pre, post, version="3L", ignore.invalid=TRUE)$UA, 0.61)
@@ -13,12 +13,13 @@ test_that("eqpchc five digit gives correct answer", {
 
 test_that("eqps throws error", {
   expect_error(ps(pre, post, version="3L", ignore.invalid=F))
+  expect_error(ps(pre[-1,], post, version="3L", ignore.invalid=TRUE))
 })
 
 pre.df <- read.csv("../testdata/pre_df.csv")
 post.df <- read.csv("../testdata/post_df.csv")
 
-test_that("eqpchc data.frame gives correct answer", {
+test_that("eqps data.frame gives correct answer", {
   expect_equal(ps(pre, post, version="3L", ignore.invalid=TRUE)$MO, 0.59)
   expect_equal(ps(pre, post, version="3L", ignore.invalid=TRUE)$SC, 0.62)
   expect_equal(ps(pre, post, version="3L", ignore.invalid=TRUE)$UA, 0.61)
@@ -26,17 +27,8 @@ test_that("eqpchc data.frame gives correct answer", {
   expect_equal(ps(pre, post, version="3L", ignore.invalid=TRUE)$AD, 0.56)  
 })
 
-# 
-# test_that("eqpchc data.frame gives correct answer", {
-#   expect_equal(pchc(pre.df, post.df, version="3L", no.problems=TRUE, totals=TRUE), res1)
-#   expect_equal(pchc(pre.df, post.df, version="3L", no.problems=TRUE, totals=FALSE), res2)
-#   expect_equal(pchc(pre.df, post.df, version="3L", no.problems=FALSE, totals=FALSE), res3)
-#   expect_equal(pchc(pre.df, post.df, version="3L", no.problems=FALSE, totals=TRUE), res4)
-# })
-# 
-# test_that("eqpchc data.frame throws error", {
-#   expect_error(pchc(pre.df, post.df, version="3L", dimensions=c("M0","SC","UA","PD","AD")))
-#   expect_error(pchc(pre.df, post.df, version="3L", ignore.invalid=FALSE))
-#   expect_error(pchc(pre.df[-1,], post.df, version="3L", ignore.invalid=FALSE))
-# })
-
+test_that("eqps data.frame throws error", {
+  expect_error(ps(pre.df, post.df, version="3L", dimensions=c("M0","SC","UA","PD","AD")))
+  expect_error(ps(pre.df, post.df, version="3L", ignore.invalid=FALSE))
+  expect_error(ps(pre.df[-1,], post.df, version="3L", ignore.invalid=TRUE))
+})
