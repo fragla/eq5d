6e395e1938b02e3ec0390f5a16e6e19fa1ec1789
Fraser Morton <fraser.morton@glasgow.ac.uk>
2023-01-17 14:51:52 +0000

    hsdi function, docs and tests added.

---
+++ R/eqhsdi.R
@@ -0,0 +1,32 @@
+#' Calculate the Health State Density Index
+#' 
+#' Calculate the Health State Density Index (HSDI) for an EQ-5D dataset.
+#' 
+#' @param scores scores data.frame, numeric or character. For data.frame default column 
+#' names should be MO, SC, UA, PD and AD representing Mobility, Self-care, 
+#' Usual activities, Pain/discomfort and Anxiety/depression. Vector using five 
+#' digit format can also be used.
+#' @param version string of value "3L" or "5L" to indicate instrument version.
+#' @param ignore.invalid booloean whether to ignore invalid scores. TRUE returns NA, FALSE throws an 
+#' error.
+#' @param digits numeric specifying the number of decimal places for percentages. Defaults to 1, 
+#' use NULL to skip rounding.
+#' @param ... character vector, specifying "dimensions" column names. Defaults 
+#' are "MO", "SC", "UA", "PD" and "AD".
+#' @return numeric containing the HSDI value.
+#' @export
+hsdi <- function(scores, version, ignore.invalid=TRUE, digits=2) {
+  cf <- eq5dcf(scores, version=version, ignore.invalid=ignore.invalid, proportions=TRUE, digits=NULL)
+  cf$CumulativeState <- 1:nrow(cf) / nrow(cf)
+  
+  cf$X <- sapply(1:nrow(cf), function(x){cf$CumulativeProp[x]-ifelse(x==1, 0, cf$CumulativeProp[x-1])})
+  cf$Y <- sapply(1:nrow(cf), function(x){cf$CumulativeState[x]+ifelse(x==1, 0, cf$CumulativeState[x-1])})
+  
+  hsdi <- sum(cf$X*cf$Y)
+  
+  if(!is.null(digits)) {
+    hsdi <- round(hsdi, digits)
+  }
+  
+  return(hsdi)
+}
---
+++ tests/testthat/test-eqhsdi.R
@@ -0,0 +1,11 @@
+context("EQ-5D HSDI")
+
+pre <- read.csv("../testdata/pre.csv")$x
+
+test_that("eqhsdi five digit gives correct answer", {
+  expect_equal(hsdi(pre, version="3L"), 0.56)
+})
+
+test_that("eqhsdi throws error", {
+  expect_error(hsdi(pre, version="3L", ignore.invalid=FALSE))
+})
