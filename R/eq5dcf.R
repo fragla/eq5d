0dfe8d51f9a22cf710a360a0b8ba6fa96bd237b8
Fraser Morton <fraser.morton@glasgow.ac.uk>
2023-01-17 14:51:32 +0000

    proportions argument added to add Proportions (between 0 and 1) and Cumulative proportions columns.

--- R/eq5dcf.R
+++ R/eq5dcf.R
@@ -6,36 +6,39 @@
 #' @param data data.frame with names MO, SC, UA, PD and AD representing
 #'   Mobility, Self-care, Usual activities, Pain/discomfort and Anxiety/depression.
 #' @param version string of value "3L" or "5L" to indicate instrument version.
-#' @param ignore.invalid whether to ignore invalid scores. TRUE returns NA, FALSE throws an 
+#' @param ignore.invalid booloean whether to ignore invalid scores. TRUE returns NA, FALSE throws an 
 #' error.
+#' @param proportions boolean whether to include proportion data columns Proportions and
+#' CumulativeProp. Default is FALSE.
+#' @param digits  numeric specifying the number of decimal places for percentages. Defaults to 1.
 #' @param ... character vector, specifying "dimensions" column names. Defaults 
 #' are "MO", "SC", "UA", "PD" and "AD".
 #' @return a data.frame or list of data.frames of counts/percentages. Columns 
 #' contain dimensions names and rows the EQ-5D score.
 #' @export
-eq5dcf <- function(data, version, ignore.invalid, ...) {
+eq5dcf <- function(data, version, ignore.invalid, proportions, digits, ...) {
   UseMethod("eq5dcf", data)
 }
 
 #' @export
-eq5dcf.data.frame <- function(data, version, ignore.invalid=TRUE, digits=1, ...) {
+eq5dcf.data.frame <- function(data, version, ignore.invalid=TRUE, proportions=FALSE, digits=1, ...) {
   args <- list(...)
   
   dimensions <- .getDimensionNames()
   if(!is.null(args$dimensions)) {dimensions <- args$dimensions}
   
   states <- getHealthStatesFromDimensions(data, version, ignore.invalid, dimensions)
-  eq5dcf.default(states, version, ignore.invalid, digits=1, ...)
+  eq5dcf.default(states, version, ignore.invalid, proportions, digits=digits, ...)
 }
 
 #' @export
-eq5dcf.matrix <- function(data, version, ignore.invalid=TRUE, digits=1, ...) {
+eq5dcf.matrix <- function(data, version, ignore.invalid=TRUE, proportions=FALSE, digits=1, ...) {
   data <- as.data.frame(data)
-  eq5dcf.data.frame(data, version, ignore.invalid=TRUE, digits=1, ...)
+  eq5dcf.data.frame(data, version, ignore.invalid, proportions, digits, ...)
 }
 
 #' @export
-eq5dcf.default <- function(data, version, ignore.invalid=TRUE, digits=1, ...) {
+eq5dcf.default <- function(data, version, ignore.invalid=TRUE, proportions=FALSE, digits=1, ...) {
   invalid.idx <- which(!data %in% getHealthStates(version))
   
   if(length(invalid.idx) > 0) {
@@ -48,17 +51,30 @@ eq5dcf.default <- function(data, version, ignore.invalid=TRUE, digits=1, ...) {
 
   #State, Frequency, Percentage, Cumulative frequency, Cumulative percentage
   frequencies <- sort(table(data), decreasing=TRUE)
-  percentage <- prop.table(as.numeric(frequencies))*100
+  percentage <- prop.table(as.numeric(frequencies)) * 100
   cum.freq <- cumsum(as.numeric(frequencies))
-  cum.perc <- cum.freq/sum(frequencies)*100
+  cum.perc <- cum.freq/sum(frequencies) * 100
+  
+  if(proportions) {
+    prop <- prop.table(as.numeric(frequencies))
+    cum.prop <- cum.freq/sum(frequencies)
+  }
   
   if(!is.null(digits)) {
     percentage <- round(percentage, digits)
     cum.perc <- round(cum.perc, digits)
   }
   
-  return(data.frame(State=names(frequencies), Frequency=as.numeric(frequencies),
-                    Percentage=percentage, CumulativeFreq=cum.freq, CumulativePerc=cum.perc, 
-                    stringsAsFactors=FALSE))
+  if(proportions) {
+    cf <- data.frame(State=names(frequencies), Frequency=as.numeric(frequencies),
+               Percentage=percentage, Proportions=prop, CumulativeFreq=cum.freq, CumulativePerc=cum.perc, 
+               CumulativeProp=cum.prop, stringsAsFactors=FALSE)
+  } else {
+    cf <- data.frame(State=names(frequencies), Frequency=as.numeric(frequencies),
+                     Percentage=percentage, CumulativeFreq=cum.freq, CumulativePerc=cum.perc, 
+                     stringsAsFactors=FALSE)
+  }
+  
+  return(cf)
 }
 
