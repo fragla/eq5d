e27adef5a030d3ececfafdaf0abc137d1604510e
fragla <fraser.morton@glasgow.ac.uk>
2023-04-13 09:01:20 +0100

    Additional eq5dcf tests added for data.frame using State column.

--- tests/testthat/test-eq5dcf.R
+++ tests/testthat/test-eq5dcf.R
@@ -14,15 +14,18 @@ test_that("eq5dcf five digit throws error", {
 })
 
 dat.df <- read.csv("../testdata/pre_df.csv")
+dat.df.state <- data.frame(State=getHealthStatesFromDimensions(dat.df, version="3L"))
 dat.df.nam <- data.frame(Mob=dat.df$MO, SelCa=dat.df$SC, UsAct=dat.df$UA,PDis=dat.df$PD, AnxDep=dat.df$AD)
 
 test_that("eq5dcf data.frame gives correct answer", {
   expect_equal(eq5dcf(dat.df, version="3L", ignore.invalid=TRUE), res)
+  expect_equal(eq5dcf(dat.df.state, version="3L", ignore.invalid=TRUE), res)
   expect_equal(eq5dcf(dat.df.nam, version="3L", ignore.invalid=TRUE, dimensions=c("Mob","SelCa","UsAct","PDis","AnxDep")), res)
 })
 
 test_that("eq5dcf data.frame throws error", {
   expect_error(eq5dcf(dat.df, version="3L", ignore.invalid=FALSE))
+  expect_error(eq5dcf(dat.df.nam, version="3L", ignore.invalid=FALSE))
   expect_error(eq5dcf(dat, version="3L", ignore.invalid=FALSE, dimensions=c("MO", "SC", "UA", "Pa", "AD")))
 })
 
