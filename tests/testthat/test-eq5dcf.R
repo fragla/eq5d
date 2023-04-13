93330b1076b636c98aa88bf8a84684a05f6737f8
fragla <fraser.morton@glasgow.ac.uk>
2023-04-13 09:55:19 +0100

    Additional test for non-standard state column name.

--- tests/testthat/test-eq5dcf.R
+++ tests/testthat/test-eq5dcf.R
@@ -15,11 +15,13 @@ test_that("eq5dcf five digit throws error", {
 
 dat.df <- read.csv("../testdata/pre_df.csv")
 dat.df.state <- data.frame(State=getHealthStatesFromDimensions(dat.df, version="3L"))
+dat.df.state2 <- data.frame(state=getHealthStatesFromDimensions(dat.df, version="3L"))
 dat.df.nam <- data.frame(Mob=dat.df$MO, SelCa=dat.df$SC, UsAct=dat.df$UA,PDis=dat.df$PD, AnxDep=dat.df$AD)
 
 test_that("eq5dcf data.frame gives correct answer", {
   expect_equal(eq5dcf(dat.df, version="3L", ignore.invalid=TRUE), res)
   expect_equal(eq5dcf(dat.df.state, version="3L", ignore.invalid=TRUE), res)
+  expect_equal(eq5dcf(dat.df.state2, version="3L", ignore.invalid=TRUE, five.digit="state"), res)
   expect_equal(eq5dcf(dat.df.nam, version="3L", ignore.invalid=TRUE, dimensions=c("Mob","SelCa","UsAct","PDis","AnxDep")), res)
 })
 
