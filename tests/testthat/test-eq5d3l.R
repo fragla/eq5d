25e207ea0c2146207ac65cdbbf0a8881e2f19a15
fragla <fraser.morton@glasgow.ac.uk>
2024-07-21 13:40:12 +0100

    Bermuda EQ-5D-3L value set added.

--- tests/testthat/test-eq5d3l.R
+++ tests/testthat/test-eq5d3l.R
@@ -18,6 +18,12 @@ test_that("EQ-5D-3L Australia TTO gives correct answer", {
   expect_equal(eq5d3l(c(MO=3,SC=3,UA=3,PD=3,AD=3), "TTO", "Australia"), -0.217)
 })
 
+test_that("EQ-5D-3L Bermuda TTO gives correct answer", {
+  expect_equal(eq5d3l(c(MO=1,SC=1,UA=1,PD=1,AD=1), "TTO", "Bermuda"), 1.000)
+  expect_equal(eq5d3l(c(MO=2,SC=2,UA=2,PD=2,AD=2), "TTO", "Bermuda"), 0.541)
+  expect_equal(eq5d3l(c(MO=3,SC=3,UA=3,PD=3,AD=3), "TTO", "Bermuda"), -0.547)
+})
+
 test_that("EQ-5D-3L Brazil TTO gives correct answer", {
   expect_equal(eq5d3l(c(MO=1,SC=1,UA=1,PD=1,AD=1), "TTO", "Brazil"), 1.000)
   expect_equal(eq5d3l(c(MO=1,SC=1,UA=2,PD=2,AD=2), "TTO", "Brazil"), 0.640)
